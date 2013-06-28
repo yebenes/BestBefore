111212a123e76fd9d485269492f6b9273ec9f316
Alberto J. Rubio <yebenes9@gmail.com>
2013-06-28 18:01:10 +0200

    Best before finished

--- src/BestBefore.groovy
+++ src/BestBefore.groovy
@@ -1,4 +1,40 @@
+import java.text.ParseException
+import java.text.SimpleDateFormat
+
+class BestBeforeException extends Exception{}
 
 class BestBefore {
 
+	def dateParser = new SimpleDateFormat("dd/MM/yy")
+
+	public BestBefore() {
+		dateParser.lenient = false
+	}
+
+	public String getBestBeforeDate(String date) {
+		try {
+			return bestDate(date)
+		} catch(Exception e) {
+			throw new BestBeforeException()
+		} 
+	}
+
+	private String bestDate(def date) {
+		def dateSplit = date.split("/")
+		def (a, b, c) = dateSplit.sort().collect() { it as int }
+		for(currentDate in [[a, b, c], [b, a, c], [c, a, b]]) {
+			def (year, month, day) = currentDate
+			if(year < 2000) { year = year + 2000 }
+			try {
+				return checkDate(day, month, year)
+			} catch(ParseException e) {}	
+		}
+		throw new BestBeforeException()
+	}
+	
+	private String checkDate(def day, month, year) {
+		String date = day + "/" + month + "/" + year 
+		dateParser.parse(date)
+		return date
+	}
 }
--- src/BestBeforeTests.groovy
+++ src/BestBeforeTests.groovy
@@ -1,6 +1,55 @@
-import static org.junit.Assert.*;
+import groovy.util.GroovyTestCase
 
+class BestBeforeTests extends GroovyTestCase {
 
-class BestBeforeTests {
+	private BestBefore bestBefore = new BestBefore();
+	
+	void testEmptyDate() {
+		try {
+			bestBefore.getBestBeforeDate("");
+			fail()
+		} catch(Exception e) {
+			assert e in BestBeforeException
+		}
+	}
+	
+	void testCorrectDate() {
+		String bestDate = bestBefore.getBestBeforeDate("01/01/2001");
+		assert bestDate == "1/1/2001"
+	}
 
+	void testCorrectDate2() {
+		String bestDate = bestBefore.getBestBeforeDate("12/31/2999");
+		assert bestDate == "31/12/2999"
+	}
+	
+	void testCorrectDate3() {
+		String bestDate = bestBefore.getBestBeforeDate("2000/11/12");
+		assert bestDate == "12/11/2000"
+	}
+	
+	void testCorrectDate4() {
+		String bestDate = bestBefore.getBestBeforeDate("1/30/0");
+		assert bestDate == "30/1/2000"
+	}
+	
+	
+	void testWrongDate() {
+		try {
+			bestBefore.getBestBeforeDate("04/07");
+			fail()
+		} catch(Exception e) {
+			assert e in BestBeforeException
+		}
+	}
+	
+	void testWrongDate2() {
+		try {
+			bestBefore.getBestBeforeDate("13, 13, 2999");
+			fail()
+		} catch(Exception e) {
+			assert e in BestBeforeException
+		}
+	}
+	
 }
